1ed2e4ed4dc5c2e1c0359cc2b2873c3a33f487db
flesentine <chrisgmcc@gmail.com>
2026-08-19 22:06:39 -0700

    Add Capacitor iOS app configuration

---
+++ capacitor.config.ts
@@ -0,0 +1,9 @@
+import type { CapacitorConfig } from '@capacitor/cli';
+
+const config: CapacitorConfig = {
+  appId: 'com.flesentine.msmadrigal',
+  appName: 'Ms. Madrigral',
+  webDir: 'www',
+};
+
+export default config;
